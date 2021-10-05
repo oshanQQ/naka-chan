f66fa876cf71c70b0f586a152cfcacea3bf6bafb
oshanQQ <oshanqq@gmail.com>
2021-10-05 23:21:03 +0900

    ✨ Add cache loop function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-use std::{collections::HashSet, env};
+use std::{
+    collections::HashSet,
+    env,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    time::Duration,
+};
 
+use chrono::offset::Utc;
 use serenity::{
     async_trait,
     framework::{
@@ -10,7 +19,11 @@ use serenity::{
         },
         StandardFramework,
     },
-    model::{channel::Message, gateway::Ready, id::UserId},
+    model::{
+        channel::Message,
+        gateway::{Activity, Ready},
+        id::{ChannelId, GuildId, UserId},
+    },
     prelude::*,
 };
 
@@ -38,7 +51,10 @@ use commands::nakachan::*;
 #[commands(nakachan)]
 struct General;
 
-struct Handler;
+struct Handler {
+    is_loop_running: AtomicBool,
+}
+
 #[async_trait]
 impl EventHandler for Handler {
     // botがメッセージを受け取った時の処理
@@ -54,6 +70,75 @@ impl EventHandler for Handler {
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
+
+    // cacheに関する処理
+    async fn cache_ready(&self, ctx: Context, _guilds: Vec<GuildId>) {
+        println!("Cache built successfully!");
+        let ctx = Arc::new(ctx);
+
+        // ループがすでに実行されていないかどうかを確認
+        if !self.is_loop_running.load(Ordering::Relaxed) {
+            let ctx1 = Arc::clone(&ctx);
+            // 並行して実行できる新しいスレッドを作成
+            tokio::spawn(async move {
+                loop {
+                    log_system_load(Arc::clone(&ctx1)).await;
+                    tokio::time::sleep(Duration::from_secs(120)).await;
+                }
+            });
+
+            // 複数のスレッドを異なるタイミングで実行
+            let ctx2 = Arc::clone(&ctx);
+            tokio::spawn(async move {
+                loop {
+                    set_status_to_current_time(Arc::clone(&ctx2)).await;
+                    tokio::time::sleep(Duration::from_secs(60)).await;
+                }
+            });
+
+            // ループが実行されているので、boolをtrueに設定。
+            self.is_loop_running.swap(true, Ordering::Relaxed);
+        }
+    }
+}
+
+async fn log_system_load(ctx: Arc<Context>) {
+    let cpu_load = sys_info::loadavg().unwrap();
+    let mem_use = sys_info::mem_info().unwrap();
+
+    // 特定のチャンネルにメッセージを送信
+    if let Err(why) = ChannelId(381926291785383946)
+        .send_message(&ctx, |m| {
+            m.embed(|e| {
+                e.title("System Resource Load");
+                e.field(
+                    "CPU Load Average",
+                    format!("{:.2}%", cpu_load.one * 10.0),
+                    false,
+                );
+                e.field(
+                    "Memory Usage",
+                    format!(
+                        "{:.2} MB Free out of {:.2} MB",
+                        mem_use.free as f32 / 1000.0,
+                        mem_use.total as f32 / 1000.0
+                    ),
+                    false,
+                );
+                e
+            })
+        })
+        .await
+    {
+        eprintln!("Error sending message: {:?}", why);
+    };
+}
+
+async fn set_status_to_current_time(ctx: Arc<Context>) {
+    let current_time = Utc::now();
+    let formatted_time = current_time.to_rfc2822();
+
+    ctx.set_activity(Activity::playing(&formatted_time)).await;
 }
 
 #[tokio::main]
@@ -68,7 +153,9 @@ async fn main() {
         .group(&GENERAL_GROUP);
 
     let mut client = Client::builder(&token)
-        .event_handler(Handler)
+        .event_handler(Handler {
+            is_loop_running: AtomicBool::new(false),
+        })
         .framework(framework)
         .await
         .expect("Err creating client");
