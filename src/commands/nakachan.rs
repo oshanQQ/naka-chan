6da5c60e3bd8492f5fb08a1fb9c5331303dd0746
3to5thpower <nisi3genki@gmail.com>
2021-10-08 00:24:16 +0900

    :green_heart: fix deploy script

--- src/commands/nakachan.rs
+++ src/commands/nakachan.rs
@@ -5,6 +5,8 @@ use serenity::prelude::*;
 #[framework::macros::command]
 #[description = "那珂ちゃんがしゃべりまーす！"]
 async fn nakachan(ctx: &Context, msg: &Message) -> framework::CommandResult {
-    msg.channel_id.say(&ctx.http, "那珂ちゃんだよー").await?;
+    msg.channel_id
+        .say(&ctx.http, "那珂ちゃんだよー:sparkles:")
+        .await?;
     Ok(())
 }
