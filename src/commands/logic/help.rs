c4ee74a7892b7519670dd0ecca6bed10abd15606
oshanQQ <oshanqq@gmail.com>
2021-10-10 23:55:28 +0900

    ✨ Display help message from txt file

--- src/commands/logic/help.rs
+++ src/commands/logic/help.rs
@@ -15,9 +15,19 @@ impl<'a> super::Command<'a> for Help<'a> {
     }
 
     async fn execute(self, _argv: &[&str]) -> anyhow::Result<Message> {
+        use std::fs::File;
+        use std::io::Read;
+        use std::path::Path;
+
         log::info!("help command requested.");
         self.log_message_detail().await;
-        self.send(self.help().as_str()).await
+
+        let path = Path::new("resources/massages.txt");
+        let mut file = File::open(&path).unwrap();
+        let mut content = String::new();
+        file.read_to_string(&mut content).unwrap();
+
+        self.send(content.to_string().as_str()).await
     }
     fn descript() -> &'static str {
         "help: This is a help command of naka-chan"
